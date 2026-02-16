37b22f33b89d2718c171a1d3a3f207e0b496061b
kavinda-100 <kavindarathnayake100@gmail.com>
2026-02-16 21:00:08 +0530

    feat: implement theme toggle and UI components with Tailwind CSS integration

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from '@/components/ModeToggle';
+
 export default function Home() {
 	return (
-		<section className="flex h-screen items-center justify-center">
+		<section className="flex h-screen items-center justify-center gap-5">
 			<h1 className="text-4xl font-bold">Hello, World!</h1>
+			<ModeToggle />
 		</section>
 	);
 }
