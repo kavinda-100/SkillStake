b6eba04987e362ab900c0ca4d3845a7f3c804c76
kavinda-100 <kavindarathnayake100@gmail.com>
2026-02-17 11:26:37 +0530

    fix: update greeting message in backend and frontend

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <section className="flex w-full h-full items-center justify-center ">
-            <h1 className="text-4xl font-bold">Hello, World!</h1>
+            <h1 className="text-4xl font-bold">Hello, World!!! 👋</h1>
         </section>
     );
 }
